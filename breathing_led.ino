0f60494c78270b7ad5372381d914a3a7fbbfdb0d
Jeremy Abbett <jeremytai@gmail.com>
2012-07-11 11:55:48 +0200

    Added the code

---
+++ breathing_led.ino
@@ -0,0 +1,21 @@
+// creating a led that breathes ala apple's laptops http://1.usa.gov/LeCBpl
+// using a 220 ohm resistor
+
+int ledPin = 3;
+int x = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int outVal = yforx(x);
+  analogWrite(ledPin, outVal);
+  delay(18); // modify the pace of breathing
+  x++;
+}
+
+int yforx(int x) {
+  return (-240*abs(sin(x*0.01)))+255; //sine wave
+}
+
